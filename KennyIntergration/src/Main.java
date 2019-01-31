3e8446ad819ed687894b04b06e8e1fc2737a3e7e
kwkenny5887 <kwkenny5887@H202R4S5.STUDENT.ad.fgcu.edu>
2019-01-31 15:42:44 -0500

    initial commit

---
+++ KennyIntergration/src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+// Kevin Kenny
+public class Main {
+
+  public void main(String[] args) {
+    
+  
+  
+  
+  
+  {
+	int num2 = 1; 
+  }	
+	/* A program integrating skills learned in COP 2006 like print outputs, and using 
+	primitive data types such as int,double, and string.*/
+	public static void main(String[] args) {
+		
+	  System.out.println("Welcome to the intergration project of Kevin Kenny!");
+		
+        int int1 = 2;
+        double double1 = 2.0;
+        String string1 = "2";
+
+        int int2 = 2;
+        double double2 = 2.0;
+        String  string2 = "2";
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println("The result of + with ints is " + (int1 + int2));
+        /* Print the sum of the double variables on a new line. */
+        System.out.println("The result of + with doubles is " + (double1 + double2));            
+        /* Concatenate and print the String variables on a new line. */
+        System.out.println("The result of + with Strings is " + (string1 + string2));
+    }	
+	
+	Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        // Complete this line
+        // Complete this line
+    }
+}
+// control shift f
+// any time you go from numbers to strings clear out buffer
+
+// variable-A location in memory
+// scope- A variable is only accessible in the method it was declared in
+
+
+class Clock {
+  // fields
+  private int angleOfHourHand;
+  private int angleOfMinuteHand;
+
+
+  // methods
+  public void tellTime() {}
+}
+
