ad4636ab87f54e066895e055e8a0240ec1724d19
Kevin <Kevin@10.0.0.214>
2019-04-28 17:12:27 -0400

    cleaned up all warnings of Main

--- KennyIntergration/src/Main.java
+++ KennyIntergration/src/Main.java
@@ -269,16 +269,20 @@ public class Main {
     switch (num + 2) {
       case 1: {
         System.out.println("Case1: Value is: " + num);
+        break;
       }
       case 2: {
         System.out.println("Case2: Value is: " + num);
+        break;
       }
       case 3: {
         System.out.println("Case3: Value is: " + num);
+        break;
       }
       default: {
         System.out.println("Default: Value is: " + num);
         System.out.println("================================");
+        break;
       }
     }
   }
